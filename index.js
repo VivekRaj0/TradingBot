951978579c792a0cca6846f263398466151f89f9
VivekRaj0 <vivekraj152089@gmail.com>
2024-10-04 19:38:16 +0530

    server and endpoint created

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+// import stockPrices from './data';
+// import { executeTrade, getTradeSummary } from './tradingLogic';
+
+const app = express();
+const PORT = 3000;
+
+// Route to simulate trading bot execution
+app.get('/run-bot', (req, res) => {
+    executeTrade(stockPrices);
+    res.json(getTradeSummary());
+});
+
+// Route to check the trade summary
+app.get('/summary', (req, res) => {
+    res.json(getTradeSummary());
+});
+
+app.listen(PORT, () => {
+    console.log(`Trading bot running on PORT:${PORT}`);
+});
